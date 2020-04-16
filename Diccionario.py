a2e39af575ba2a8ebbd252782e25d0ee3f0455fd
LuisLegreaux39 <lleg231196r@gmail.com>
2020-04-17 00:38:50 +0200

    Organizando parrafo en  wikipedia_word_search(palabra) para que se adapte a la ventana

--- Diccionario.py
+++ Diccionario.py
@@ -1,6 +1,7 @@
 # Este programa debe buscar el significado de una palabra en wikipedia con la api de python
 # e ir haciendo una base de datos en un excel de todas las palabras buscadas con su significado
 import tkinter as TK
+from tkinter import messagebox
 from partials import *
 from wikipediaapi import Wikipedia
 import os
@@ -9,32 +10,34 @@ import os
 padx = 10
 pady = 10
 bg = 'royal blue'
-win = TK.Tk()
-win.title("Traductor")
-win.geometry('300x100')
-win.config(bg='royal blue')
-labelTitular = TK.Label(
-win, text="¿Qué palabra quieres buscar?", font=5, padx=padx, bg=bg)
-labelTitular.pack()
-entryPalabra = TK.Entry(win, width=32)
-entryPalabra.pack(pady=pady)
-btnTraducir = TK.Button(win, text="Traduce", width=15,command=print("Hello"))
-btnTraducir.pack()
-
+def main():
+    win = TK.Tk()
+    win.title("Traductor")
+    win.geometry('300x100')
+    win.config(bg='royal blue')
+    labelTitular = TK.Label(
+    win, text="¿Qué palabra quieres buscar?", font=5, padx=padx, bg=bg,fg='white')
+    labelTitular.pack()
+    entryPalabra = TK.Entry(win, width=32)
+    entryPalabra.pack(pady=pady)
+    btnTraducir = TK.Button(win, text="Traduce", width=15,command=lambda:mostrarDefinicion(entryPalabra.get()))
+    btnTraducir.pack()
+    win.mainloop()
 
 # main()
 def DefinitionWin(titutlo,resultado):
     titulo = str(titutlo)
     resultado = str(resultado)
     Definition = TK.Tk()
-    Definition.geometry("200 x 400")
+    Definition.geometry("300x400")
     Definition.title('Definicion')
     Definition.config(bg=bg)
-    Titulo = TK.Label(Definition,text =titulo,font=15)
-    Titulo.pack(justify='center',pady=pady)
-    
-    
-
+    Titulo = TK.Label(Definition,text=titulo,font=('Times New Roman ',20),justify='center',bg=bg,fg='white')
+    Titulo.pack(pady=2)
+    definicionBody= TK.LabelFrame(Definition,bg=bg,text="definicion")
+    definicionBody.pack(fill='both',expand='yes',padx=padx,pady=5)
+    textoBody = TK.Label(definicionBody,text=resultado,justify='center',bg=bg,fg='white',wraplength=350)
+    textoBody.pack(pady=2,padx=5)
 ############# Funciones #########################
 def wikipedia_word_search(palabra):
     palabra = str(palabra)
@@ -48,8 +51,13 @@ def wikipedia_word_search(palabra):
         ExceptionLogCreator(e)
     return resultado
 
-def mostrarDefinicion():
-    
+def mostrarDefinicion(Palabra_Buscada):
+        titulo = str(Palabra_Buscada)
+        if(len(titulo)<1):
+            messagebox.showerror(title='Campo Vacion',
+                                    message="Debe rellenar el campo d palabra")
+        else:
+            resultado=wikipedia_word_search(titulo)
+            DefinitionWin(titulo,resultado)
+main()
 
-
-win.mainloop()
